c32c3d4f4d834a0fb4f31dcf6331da4aba4b5b8b
Amanda Buttineau <114027684+ab1820@users.noreply.github.com>
2023-01-17 12:00:58 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "index.html", "script.js"],
+  theme: {
+    extend: {
+      colors: {
+        background: "url(../images/background.png)",
+        hoverColor: "#4d7b93",
+      },
+      fontFamily: {
+        headings: ["Marck\\ Script"],
+        body: ["Share\\ Tech\\ Mono"],
+      },
+      screens: {
+        xsm: "320px",
+        odd: "800px",
+      },
+    },
+  },
+  plugins: [],
+};
